e3e822711ba4e34b08041f82f742e1d79026620e
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-03-03 19:47:16 +0600

    i added here new file and added comparision

---
+++ D. Chapter-4 (comparison)/1. Anti/task-2.js
@@ -0,0 +1,6 @@
+/* 
+২. তোর ক্লাসে 45 জন ছাত্র আছে, আর পাশের ক্লাসে 50 জন। একটা প্রোগ্রাম লিখে দেখ, কোন ক্লাসে বেশি ছাত্র আছে।
+*/
+let myClassStudents = 45;
+let anotherClassStudents = 50;
+console.log(myClassStudents < anotherClassStudents);
---
+++ D. Chapter-4 (comparison)/2. Variable Tuilona/task-2.js
@@ -0,0 +1,6 @@
+/* 
+২. একটা পরীক্ষায় পাস মার্কস 33 ধর, তুই 45 পেয়েছিস। চেক কর, তুই পাস করেছিস কি না
+*/
+let passMarks = 33;
+let myMarks = 45;
+console.log(passMarks <= myMarks);
