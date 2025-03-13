347fc76c90d85d151ae0e947da28fbc0d4ccfd2e
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-03-13 12:47:52 +0600

    file updated

--- E. Chapter-5 (Condition)/1. Condition no Confusion/task-5.js
+++ E. Chapter-5 (Condition)/1. Condition no Confusion/task-5.js
@@ -1,8 +1,8 @@
 /* 
 ৫. যদি মুভির শো রাত ৭টার আগে হয়, তাহলে তুই মুভি দেখবি। যদি পরে হয়, তাহলে বাসায় গিয়ে নাকে তেল দিয়ে ঘুমাবি। এর জন্য প্রোগ্রাম লিখ।
  */
-let movieTime = '7:00 PM';
-if (movieTime < '6:00 PM') {
+let movieTime = 6;
+if (movieTime < 7) {
     console.log("Let's go to Watch Movie");
 }
 else {
